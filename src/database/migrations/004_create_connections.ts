cfebbabb4f38830d68e6d0e099c46f8d997313d7
Ricardo <ricardo.dalcinh@gmail.com>
2020-08-04 10:40:40 -0300

    Fix connections migration timestamp

--- src/database/migrations/004_create_connections.ts
+++ src/database/migrations/004_create_connections.ts
@@ -3,7 +3,10 @@ import Knex from 'knex'
 export async function up(knex: Knex) {
   return knex.schema.createTable('connections', (table) => {
     table.increments('id').primary()
-    table.timestamp('created_at').defaultTo('now()').notNullable()
+    table
+      .timestamp('created_at')
+      .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+      .notNullable()
 
     table
       .integer('user_id')
