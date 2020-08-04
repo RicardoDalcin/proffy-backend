67d97e1e1c9eaf229fb81e6d865e3f5d0a7b0a2e
Ricardo <ricardo.dalcinh@gmail.com>
2020-08-04 10:47:26 -0300

    Classes and connections controllers + CORS setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express'
+import cors from 'cors'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (req, res) => {
-  console.log('Rota')
-  return res.send('Hello world')
-})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
